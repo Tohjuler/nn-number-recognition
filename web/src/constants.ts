752d8918ba81485e07211a902f210f58152784e2
Tohjuler <tobias@tohjuler.dk>
2026-03-16 15:23:32 +0100

    feat: add Full model (100 epochs)

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -3,6 +3,7 @@ export const MODEL_INPUT_SIZE = 28;
 export const MODEL_VECTOR_LENGTH = MODEL_INPUT_SIZE * MODEL_INPUT_SIZE;
 export const MODELS = {
 	"Full model (20 epochs)": "/models/network-full-20.json",
+	"Full model (100 epochs)": "/models/network-full-100.json",
 	"Full model (low lr, 10 epochs)":
 		"/models/network-full-shuffle-low-lr-10.json",
 };
